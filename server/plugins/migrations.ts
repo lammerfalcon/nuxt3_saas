e42585bf6522248590eabd1bfac630970eb080bc
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-06-15 19:06:42 +0300

    init

--- server/plugins/migrations.ts
+++ server/plugins/migrations.ts
@@ -1,17 +1,16 @@
-// import { consola } from 'consola'
-// import { migrate } from 'drizzle-orm/d1/migrator'
+import { consola } from 'consola'
+import { migrate } from 'drizzle-orm/d1/migrator'
 
 export default defineNitroPlugin(async () => {
   if (!import.meta.dev) return
 
-  onHubReady(() => {
-    console.log('Migrating database', e)
-    // await migrate(useDrizzle(), { migrationsFolder: 'server/database/migrations' })
-    //   .then(() => {
-    //     consola.log('Database migrations done')
-    //   })
-    //   .catch((err) => {
-    //     consola.error('Database migrations failed', err)
-    //   })
+  onHubReady(async () => {
+    await migrate(useDrizzle(), { migrationsFolder: 'server/database/migrations' })
+      .then(() => {
+        consola.log('Database migrations done')
+      })
+      .catch((err) => {
+        consola.error('Database migrations failed', err)
+      })
   })
 })
