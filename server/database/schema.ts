1dc097b9f6db30a839c0b10cfab19b4eaa79caec
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-06-15 12:02:23 +0300

    init

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -2,5 +2,9 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
-  email: text('email').notNull().unique()
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  avatar: text('avatar').notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull()
 })
